4bb823188b08d207d755b1fbf50a503e2dc71804
MADARAdev <88511184+Letrider@users.noreply.github.com>
2024-08-03 19:57:18 +0300

    Edited slide

--- src/components/Slide/index.tsx
+++ src/components/Slide/index.tsx
@@ -1,9 +1,10 @@
 import { ISlide } from '@/types/types'
+import './index.scss'
 
 export default function Slide({ url, description, isCurrent }: ISlide) {
 	return (
-		<>
-			{isCurrent && <img src={url} alt={description} />}
-		</>
+		<div className='slide'>
+			{<img src={url} alt={description} />}
+		</div>
 	)
 }
